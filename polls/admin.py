e53fdb5553e590ee4f0c22ec3ff5ac9eee1ac6d1
Jinqiang Yuan <yuanjinq@gmail.com>
2019-04-13 00:55:29 +0800

    final version for Django Tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question,Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -7,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
