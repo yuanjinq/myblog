25aefb51a94acde8e63a05560f271f53a9c49ba9
Jinqiang Yuan <yuanjinq@gmail.com>
2019-04-13 17:32:47 +0800

    A workable blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 app_name='blog'
 urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('post_list/', views.post_list, name='post_list'),
+    #path('', views.index_view, name='index'),
+    #path('post_list/', views.post_list, name='post_list'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('posts/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('posts/', views.PostListView.as_view(), name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-
+from django.views import generic
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_post_list'
 
-def index_view(request):
-    #return HttpResponse("Hello World! From Jerry Coding")
-    return render(request, 'blog/index.html')
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Post.objects.filter(
+            published_date__lte=timezone.now()
+        ).order_by('-published_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Post.objects.filter(published_date__lte=timezone.now())
+    
+class PostListView(generic.ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'latest_post_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Post.objects.filter(
+            published_date__lte=timezone.now()
+        ).order_by('-published_date')   
